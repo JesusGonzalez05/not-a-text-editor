9f70fc6866939b2a3fa45f7959bfb5d8a8599c84
JesusGonzalez05 <Jesusgonzalez051698@gmail.com>
2022-12-05 21:44:11 -0500

    able to get web pack to bundle items need to work on making sure pluggins spin up.

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -14,7 +14,7 @@ const initdb = async () =>
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
-  // console.log('Post to the ase');
+  // console.log('Post to the DB');
   const jateDb = await openDB('jate', 1);
   const tx = jateDb.transaction('jate', 'readwrite');
   const store = tx.objectStore('jate');
@@ -34,5 +34,5 @@ export const getDb = async () => {
   console.log('result.value', result);
   return result;
 };
-
+// starts DB
 initdb();
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -22,16 +22,28 @@ module.exports = () => {
         template: './index.html',
         title: 'Webpack Plugin',
       }),
-      new MiniCssExtractPlugin(),
       new InjectManifest({
-        swSrc: './src/sw.js',
+        swSrc: './src-sw.js',
         swDest: 'src-sw.js',
       }),
     ],
 
     module: {
       rules: [
-        
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /(node_modules|bower_components)/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env']
+            }
+          }
+        }
       ],
     },
   };
